f86cfd4ce91ec7728837acdbecd510594c5bafc4
ousseynouba <papaousseynouba@esp.sn>
2026-09-24 17:09:47 +0200

    nouveau test pull request conflict

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@ int main() {
    cout << "J'ai déja codé en php" << endl;
    cout << "J'aime le rugby" << endl;
    cout << "La programmmation sert à résoudre les problèmes" << endl;
+   cout << "Test ousseynou Ba" << endl;
    cout << "Fin du lab 1" << endl;
 
 
